e69f089c0db9a439c4979349c13db19cbe3fcc7d
Sybren Maréchal <sybrenmarechal@Sybrens-MacBook-Pro-9.local>
2019-02-16 00:18:53 +0200

    change path name

--- routes/qrReader.js
+++ routes/qrReader.js
@@ -1,6 +1,6 @@
 var express     =   require("express");
 var bodyParser  =   require("body-parser");
-var mongoOp     =   require("../models/qrReaderModel");
+var mongoOp     =   require("../models/RFIDReaderModel");
 var router      =   express.Router();
 
 router.use(bodyParser.json());

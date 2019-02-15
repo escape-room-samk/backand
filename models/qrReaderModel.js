c0563e785d9345e3e63222920071839dffa94cc8
Sybren Maréchal <sybrenmarechal@Sybrens-MacBook-Pro-9.local>
2019-02-15 23:28:25 +0200

    REST interface and database for QR reader

---
+++ models/qrReaderModel.js
@@ -0,0 +1,10 @@
+var mongoose    =   require("mongoose");
+mongoose.connect('mongodb://localhost:27017/demoDb', { useNewUrlParser: true });
+// create instance of Schema
+var QRschema = mongoose.Schema({
+    "devID" : String,
+    "QRMessage": String
+});
+
+// create model if not exists.
+module.exports = mongoose.model('QRschema',QRschema);

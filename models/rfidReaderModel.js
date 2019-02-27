a0f133997742d42483a3bdff895b35cb65f178ad
sybren-marechal <sybrenmarechal@icloud.com>
2019-02-27 18:19:57 +0200

    connect

--- models/rfidReaderModel.js
+++ models/rfidReaderModel.js
@@ -1,10 +1,10 @@
 var mongoose = require("mongoose");
-mongoose.connect("mongodb://127.0.0.1:27017", { useNewUrlParser: true });
+mongoose.connect("mongodb://localhost:27017/myapp"||"mongodb://127.0.0.1:27017/myapp", { useNewUrlParser: true });
 // create instance of Schema
 var rfidschema = mongoose.Schema({
   devID: String,
   RFIDMessage: String,
-}); 
- 
+});
+
 // create model if not exists.
 module.exports = mongoose.model("rfidschema", rfidschema);

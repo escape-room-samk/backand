705d77d790a117b0f82eb2365081fb05ca4ba420
sybren-marechal <sybrenmarechal@icloud.com>
2019-03-05 18:04:06 +0200

    change database

--- models/qrReaderModel.js
+++ models/qrReaderModel.js
@@ -1,5 +1,5 @@
 var mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/demoDb", { useNewUrlParser: true });
+mongoose.connect("mongodb://localhost:27017/test", { useNewUrlParser: true });
 // create instance of Schema
 var QRschema = mongoose.Schema({
   devID: String,
